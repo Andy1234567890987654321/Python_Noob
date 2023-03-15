0fb713f1a33c9ca1968c3e610352572ba09fab5b
冠安 <126768807+Andy1234567890987654321@users.noreply.github.com>
2023-03-15 15:08:40 +0000

    homework2

--- class2/homework_2.py
+++ class2/homework_2.py
@@ -4,18 +4,22 @@
 2.	請問使用什麼function可以顯示出形態
 答案:
 3.	請問 a = int('1'), a的形態是什麼
-答案:
+答案:1sssss
 4.	請問 b = str(1), b的形態是什麼
-答案:
+答案:字串
 5.	請問 c = float(1), c的形態是什麼
-答案:
+答案:1.0
 6.	請問 d = bool(1), d的形態是什麼
-答案:
+答案:True
 7.	請列出Python加、減、乘、除表示符號
-答案:
+答案:+-*/
 8. 請問今天學了哪一些function(函式)?
-答案:
+答案:max/len/type/int/print
 9. 延續上題, 請嘗試描述每個function的功能各別是什麼?
-答案:
+答案:max=取某幾個數字最大的
+    len=在字串裡有幾個字
+    type=判斷它是bool or str or float or int
+    int=什麼東西轉成為整數
+    print=顯示
 
 '''
