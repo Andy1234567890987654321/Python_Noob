e91051ff7e10b0711bee7f6a02ea4494d0192996
Andy1234567890987654321 <0117cube@gmail.com>
2023-04-13 18:36:34 +0800

    wertyuopsdfgklxcvbnm,.

---
+++ class7/homework 7.py
@@ -0,0 +1,11 @@
+'''
+# 星形中心為正午邊形，每一個外角為360度/=72度
+# 星形每隻銳角的角度=180度-2*72度=36度
+# turtle.pensize(5)  # 線徑寬度1~10
+# turtle.pencolor("yellow")  # 線的顏色
+# turtle.fillcolor("yellow")  # 區域填滿顏色
+# turtle.begin_fill()  # 填滿區域設定開始
+# turtle.end_fill()  # 填滿區域設定結束
+
+在slack尚有第二題的題目
+'''
