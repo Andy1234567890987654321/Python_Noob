8218332b4ae4a0b9d0d9036afe85ec13f055515c
Andy1234567890987654321 <0117cube@gmail.com>
2023-04-13 17:39:00 +0800

    class6

---
+++ class6/class6.py
@@ -0,0 +1,38 @@
+# import turtle
+# applebananaorange = 100
+# applebananaorangegrape = 90
+# applebananaorangegrapepen = 100000000
+# applebananaorangegrapepencil = 10000000000
+# turtle.speed(10)
+# turtle.forward(applebananaorange)
+# turtle.backward(applebananaorangegrape)
+# turtle.right(applebananaorangegrapepen)
+# turtle.left(applebananaorangegrapepencil)
+# turtle.done()
+
+# import turtle
+# turtle.speed(10)
+
+# for loop in range(4):
+#     turtle.forward(100)
+#     turtle.right(90)
+# turtle.done()
+
+
+import time
+import turtle
+
+turtle.speed(0)
+turtle.forward(100)
+turtle.backward(100)
+time.sleep(1)  # 休息1秒
+turtle.right(6)
+time.sleep(1)  # 休息1秒
+turtle.clear()
+turtle.forward(100)
+turtle.backward(100)
+time.sleep(1)  # 休息1秒
+turtle.right(6)
+time.sleep(1)  # 休息1秒
+turtle.clear()
+turtle.done()
