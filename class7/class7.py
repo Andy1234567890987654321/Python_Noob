b66035e698f78adbe154e482f3725225566a0b8d
Andy1234567890987654321 <0117cube@gmail.com>
2023-04-13 18:33:08 +0800

    homework 7

---
+++ class7/class7.py
@@ -0,0 +1,27 @@
+import turtle
+# turtle.color('blue') #設定烏龜顏色
+# turtle.shape('turtle')#設定小烏龜'arrow', 'turtle', 'circle', 'square','triangle','classic'
+# turtle.stamp() #蓋章
+# turtle.penup() #提筆
+#######################################################################################################################################
+# turtle.penup()
+# turtle.color('red')
+# turtle.shape('turtle')
+# applebananaorange = 0
+# turtle.speed(0)
+# for loop in range(5, 2001, 2):
+#     turtle.stamp()
+#     turtle.right(25)
+#     turtle.forward(loop)
+#     turtle.stamp()
+#     applebananaorange += 1
+#     print(applebananaorange)
+# turtle.done()
+#######################################################################################################################################
+# 星形中心為正午邊形，每一個外角為360度/=72度
+# 星形每隻銳角的角度=180度-2*72度=36度
+# turtle.pensize(5)  # 線徑寬度1~10
+# turtle.pencolor("yellow")  # 線的顏色
+# turtle.fillcolor("yellow")  # 區域填滿顏色
+# turtle.begin_fill()  # 填滿區域設定開始
+# turtle.end_fill()  # 填滿區域設定結束
