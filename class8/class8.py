281cb041bfc963f7027ac2f710faa87c71765106
Andy1234567890987654321 <0117cube@gmail.com>
2023-04-20 18:33:58 +0800

    homework 8

---
+++ class8/class8.py
@@ -0,0 +1,26 @@
+# applebananaorange = int(input("請輸入隨便一個數字"))
+# mus = 0
+# for loop in range(applebananaorange+1):
+#     mus = mus+loop
+# print(f'從0加到{applebananaorange}={mus}')
+
+# a = (applebananaorange+1)*applebananaorange/2
+# print(a)
+
+# for loop in range(1, 101):
+#     for pool in range(1, 101):
+#         print(f"{loop}x{pool}={loop*pool} ", end='')
+#     print()
+
+# 1 ()
+# 2 **次方ˋ直
+# 3 正負數
+# 4 乘法 * 小數除法 / 整數除法 // 取餘數 ％
+# 5 加法 減法
+# 6 == != >= <= > <
+# 7 not and or
+# 8 = += -=*= /= %= //= **=
+loop = 0
+while loop < 100001:
+    print(loop)
+    loop += 1
