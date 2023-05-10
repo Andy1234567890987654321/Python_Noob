714d0cb704f6d45b17cdfd6fc56f67cb0a4fb1cd
Andy1234567890987654321 <0117cube@gmail.com>
2023-05-10 22:28:41 +0800

    我呸厲害有做功課

---
+++ class9/homework9.py
@@ -0,0 +1,34 @@
+"""
+輸入一數字n為尋找的區間範圍, 找出區間範圍
+3,7的倍數顯示在螢幕上, 其餘不顯示
+
+hint:可以使用%取餘數進行判斷
+
+EX
+請輸入正整數:20
+3
+6
+7
+9
+12
+14
+15
+18
+"""
+while True:
+    aabbccddeeffghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ = input(
+        '數字輸入:')
+    if aabbccddeeffghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ < "5"
+    print("3")
+    if aabbccddeeffghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ <= "6"
+    print("3,6")
+    if aabbccddeeffghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ <= "8"
+    print("3,6,7")
+    if aabbccddeeffghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ <= "11"
+    print("3,6,7,9")
+    if aabbccddeeffghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ <= "13"
+    print("3,6,7,9,12")
+    if aabbccddeeffghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ <= "15"
+    print("3,6,7,9,12,14")
+    if aabbccddeeffghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ <= "17"
+    print("3,6,7,9,12,14,15,18")
