2e9f18752bd4faca6edeff7e6588350f420a3278
Andy1234567890987654321 <0117cube@gmail.com>
2023-07-08 11:55:33 +0800

    class17

---
+++ class17/class.py
@@ -0,0 +1,17 @@
+print('I %d savage' %10)
+print('I %3d savage' %10)
+print('I %03d savage' %10)
+
+print("I {0:d} savage".format(95))
+print("I {0:3d} savage".format(95))
+print("I {0:03d} savage".format(95))
+
+print('I %f savage' %94)
+print('I %.10f savage' %94)
+
+print('I {0:f} savage'.format(44))
+print('I {0:.2f} savage'.format(44))
+
+print('I %s savage' %"94")
+
+print('I {0:s} savage'.format("44"))
