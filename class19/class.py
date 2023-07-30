1e59bd69f130739794f53ece8d1bed264d6621d0
Andy1234567890987654321 <0117cube@gmail.com>
2023-07-30 11:39:19 +0800

    class19

---
+++ class19/class.py
@@ -0,0 +1,14 @@
+# a=eval(input('請輸入算式:'))
+# print(a,type(a))
+# import os
+# file_path="C:/Users/USER/Downloads/Python_Snake-master/Python_Snake-master/Fonts/Cascadia.ttf"
+# if os.path.isfile(file_path):
+#     print('檔案存在')
+# else:
+#     print('不存在')
+
+
+file=open('C:/Users/USER/Desktop/Python_Noob/class18/example.txt','r')
+applebananaorange=file.read()
+print(applebananaorange)
+file.close()
